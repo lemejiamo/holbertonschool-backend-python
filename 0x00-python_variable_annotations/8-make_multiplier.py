5da348d361eb7cc6db8063fbc5cbc60c3cd4dc68
Luis Mejia <luismejia69@gmail.com>
2022-08-04 21:48:10 -0500

    8-make_multiplier.py

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+8. Complex types - functions
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Write a type-annotated function make_multiplier that takes
+    a float multiplier as argument and returns a
+    function that multiplies a float by multiplier.
+    """
+    def _multiplier(number: float) -> float:
+        """
+        function that multiplies a float by multiplier.
+        """
+        return number * multiplier
+
+    return _multiplier
