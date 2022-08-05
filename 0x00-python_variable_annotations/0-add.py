2dc8bb8e548a30dcd9078e6170579781748fa7d3
Luis Mejia <luismejia69@gmail.com>
2022-08-04 21:15:26 -0500

    basis typed

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+0 add  function add that takes a float a and a float b
+as arguments and returns their sum as a float.
+"""
+
+
+def add(a: float, b: float):
+    """
+    return the add of two numbers as float
+    """
+    return (a + b)
