200fe6e55b9bc1390604273d8175850fad8e2811
Luis Mejia <luismejia69@gmail.com>
2022-08-04 21:21:09 -0500

    concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+function concat that takes a string str1 and a string str2 
+as arguments and returns a concatenated string
+
+"""
+
+
+def concat(string1: str, string2: str) -> str:
+    """
+    concat to strings
+    """
+    return string1 + string2
