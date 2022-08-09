569afb57a321e0979ec51fc5ebd79d25d50d71f8
Luis Mejia <luismejia69@gmail.com>
2022-08-08 21:47:37 -0500

    asyncio basis

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 """
-asynchronous coroutine that takes in an integer argument
-(max_delay, with a default value of 10) named wait_random
-that waits for a random delay between 0 and max_delay
-(included and float value) seconds and eventually returns it.
+Description: asynchronous coroutine that takes in an integer argument
+             (max_delay, with a default value of 10) named wait_random
+             that waits for a random delay between 0 and max_delay
+             (included and float value) seconds and eventually returns it.
 """
 
-import ramdom
+import random
 import asyncio
 
 
-def wait_ramdom(max_delay: int = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """ wait for a ramdom time and returns the waiting time"""
-    wait = max_delay * ramdom.ramdom()
+    wait = max_delay * random.random()
     await asyncio.sleep(wait)
     return wait
