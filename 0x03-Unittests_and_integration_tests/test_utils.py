6db9d12ece42d8188c0e3c24a7227d6029a15c3a
Luis Mejia <luismejia69@gmail.com>
2022-08-29 15:02:34 -0500

    memoize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,9 +5,9 @@ Unittest module to utils module
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map as anm
-from utils import get_json
+from utils import get_json, memoize
 from typing import Mapping, Sequence, Any, Dict
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -51,3 +51,27 @@ class TestGetJson(unittest.TestCase):
             func_response = get_json(test_url)
             mock_request.assert_called_once_with(test_url)
             self.assertEquals(func_response, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    Test that when calling a_property twice, the correct result is returned but
+    a_method is only called once using assert_called_once.
+    """
+
+    def test_memoize(self):
+
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_val=42) as mock_object:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property, mock_object.return_val)
+            self.assertEqual(test_class.a_property, mock_object.return_val)
+            mock_object.assert_called_once()
